f6ca68dbebd4752a3716033f232e66888a2e969d
Enyina Matthew <enyina3848@gmail.com>
2023-03-20 23:47:52 +0100

    first commit

---
+++ src/responsive.js
@@ -0,0 +1,25 @@
+import { css } from "styled-components";
+
+export const mobile = (props) => {
+  return css`
+    ${
+      "" /* @media only screen and (max-width: 375px) {
+      ${props}
+    } */
+    }
+    @media only screen and (max-width: 414px) {
+      ${props}
+    }
+    ${
+      "" /* @media only screen and (max-width: 390px) {
+      ${props}
+    }
+    @media only screen and (max-width: 393px) {
+      ${props}
+    }
+    @media only screen and (max-width: 360px) {
+      ${props}
+    } */
+    }
+  `;
+};
