d4582d47cf031ccedbc9472b93a3f43f35071395
Enyina Matthew <enyina3848@gmail.com>
2023-03-21 00:28:20 +0100

    redeploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Badge } from "@material-ui/core";
+import { Badge } from "@mui/material";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
